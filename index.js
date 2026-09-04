f2bc4bb065f5f76ff9c07565f064dcab53c6bde2
bueno-commits <aaadamw@gmail.com>
2026-09-04 19:32:32 +0200

    Pelna podmiana z wymuszonym wylogowywaniem przy zamknieciu okna

--- index.js
+++ index.js
@@ -9,18 +9,23 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// Rygorystyczne blokowanie pamięci podręcznej (brak możliwości powrotu przyciskiem "Wstecz")
 app.use((req, res, next) => {
     res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
     next();
 });
 
+// Konfiguracja sesji tymczasowej (wygasa z zamknięciem przeglądarki)
 app.use(session({
     secret: 'tajny_klucz_pudelek',
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: null,
-        httpOnly: true
+        httpOnly: true,
+        sameSite: 'lax',
+        maxAge: null
     }
 }));
 
@@ -122,10 +127,15 @@ app.post('/change-password-o', async (req, res) => {
     }
 });
 
-app.get('/logout', (req, res) => {
+// Obsługa wylogowania (zarówno przez przycisk, jak i przy zamknięciu karty)
+app.all('/logout', (req, res) => {
     req.session.destroy(() => {
         res.clearCookie('connect.sid');
-        res.redirect('/');
+        if (req.method === 'POST') {
+            res.sendStatus(200);
+        } else {
+            res.redirect('/');
+        }
     });
 });
 
